e8498f183b9c48b64b619c076de93f6758056ba3
Fabian Friedrich <friedrich.fabian@gmx.net>
2022-07-17 15:35:08 +0200

    Fixed BuildingManager

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,6 +11,7 @@ public class BuildingManager : MonoBehaviour
     [SerializeField] private StringVariable upgradeState;
     [SerializeField] private VoidEvent itemGotPlaced;
     [SerializeField] private Tilemap tilemap;
+    [SerializeField] private LayerMask buildingLayer;
     private TilemapRenderer tilemapRenderer;
     private Vector2 worldMousePos;
 
@@ -44,10 +46,33 @@ public class BuildingManager : MonoBehaviour
         }
         preview.transform.position = buildingPos;
     }
+    
+    private bool ConvertMousePosToTilePos(out Vector3 buildingPos)
+    {
+        buildingPos = Vector3.zero;
+        Vector3Int tilePos = new Vector3Int(Mathf.RoundToInt(worldMousePos.x), Mathf.RoundToInt(worldMousePos.y), 0);
+        if (TileIsNotValid(tilePos)) return true;
+
+        buildingPos = tilePos - new Vector3(0, .5f, 0);
+        return false;
+    }
+
+    private bool TileIsNotValid(Vector3Int position)
+    {
+        if (!tilemap.HasTile(position)) return true;
+        return Physics2D.OverlapCircle((Vector3) position, .2f, buildingLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (ConvertMousePosToTilePos(out var buildingPos)) return;
+        Gizmos.DrawWireSphere(buildingPos + new Vector3(0, .5f, 0), .2f);
+    }
 
     public void OnMouseClick()
     {
-        if (selectedBuilding.Value != null)
+        if (preview != null)
         {
             PlaceBuilding();
             return;
@@ -55,17 +80,6 @@ public class BuildingManager : MonoBehaviour
 
         if (upgradeState.Value != "") UpDowngradeBuilding();
         
-    }
-
-    private void PlaceBuilding()
-    {
-        if (ConvertMousePosToTilePos(out var buildingPos)) return;
-
-        preview.GetComponentInChildren<Building.Building>().Enable();
-        preview.GetComponentInChildren<BoxCollider2D>().enabled = true;
-        preview = null;
-
-        itemGotPlaced.Raise();
     }
 
     private void UpDowngradeBuilding()
@@ -90,24 +104,16 @@ public class BuildingManager : MonoBehaviour
             buildingOb.Downgrade();
         }
     }
-
-    private bool ConvertMousePosToTilePos(out Vector3 buildingPos)
-    {
-        buildingPos = Vector3.zero;
-        Vector3Int tilePos = new Vector3Int(Mathf.RoundToInt(worldMousePos.x), Mathf.RoundToInt(worldMousePos.y) - 1, 0);
-        if (TileIsNotValid(tilePos)) return true;
-
-        buildingPos = tilePos + new Vector3(0, 0.5f, 0);
-        return false;
-    }
-
-
-    private bool TileIsNotValid(Vector3Int position)
-    {
-        if (!tilemap.HasTile(position)) return true;
-        return Physics2D.OverlapCircle((Vector3) position, .2f);
-    }
     
+    private void PlaceBuilding()
+    {
+        preview.GetComponentInChildren<Building.Building>().Enable();
+        preview.GetComponentInChildren<BoxCollider2D>().enabled = true;
+        preview = null;
+
+        itemGotPlaced.Raise();
+    }
+
     public void OnMousePositionChanged(InputValue value)
     {
         Vector2 mousePos = value.Get<Vector2>();
